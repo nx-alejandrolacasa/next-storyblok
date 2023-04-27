3667d93ccbebeed79f7ce1000fb3b0bd99068179
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-04-27 21:16:27 +0200

    Add clsx

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,13 +2,16 @@ import Link from 'next/link'
 import { storyblokEditable } from '@storyblok/react'
 import { AnchorHTMLAttributes } from 'react'
 import { ButtonStoryblok } from '@/types/sb-types'
+import clsx from "clsx";
 
 type ButtonProps = { blok: ButtonStoryblok }
 
 export function Button({ blok }: ButtonProps) {
-  const className = `p-2 border-2 border-gray-100 rounded-xl text-center ${
-    blok.type === 'primary' ? 'bg-rose-400' : 'bg-indigo-400 text-white'
-  }`
+  const className = clsx(
+    "p-2 border-2 border-gray-100 rounded-xl text-center",
+    blok.type === 'primary' && 'bg-rose-400',
+    blok.type !== 'primary' && 'bg-indigo-400 text-white'
+  )
 
   const LinkButton =
     blok.link?.linktype === 'story'
