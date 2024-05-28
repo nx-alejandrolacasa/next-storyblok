b225a6a3039bfc3709eb8cce47c4a69292257d49
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2024-05-28 09:55:37 +0200

    Dependencies upgrade

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import { fetchStoriesBySlug } from '@/utils/storyblok'
 export default async function Home() {
   const { data } = await fetchStoriesBySlug({
     slug: 'home',
-    // fetchOptions: { next: { revalidate: 3600 } },
-    // params: { resolve_relations: 'team.teamMembers' },
   })
   return <StoryblokStory story={data.story} bridgeOptions={{}} />
 }
